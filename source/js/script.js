e84fa7cb0e8298b3cc78ccd2355dff522d173abf
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2020-04-30 21:34:46 +0500

    Правки

---
+++ source/js/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".site-list");
+let menuToggle = document.querySelector(".page-header__toggle");
+
+menuToggle.addEventListener("click", function() {
+  menuToggle.classList.toggle("page-header__toggle--activated");
+  menu.classList.toggle("site-list--activated");
+});
