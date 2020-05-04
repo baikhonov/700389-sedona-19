1a7afcb4d8a301059e7ca70b87d3fe9495ef8413
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2020-05-04 15:06:46 +0500

    Начал оптимизировать векторную и растровую графику

--- source/js/script.js
+++ source/js/script.js
@@ -5,3 +5,6 @@ menuToggle.addEventListener("click", function() {
   menuToggle.classList.toggle("page-header__toggle--activated");
   menu.classList.toggle("site-list--activated");
 });
+
+// Polyfill for IE to support SVG sprite
+svg4everybody();
